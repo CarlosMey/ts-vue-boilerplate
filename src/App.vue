633d7e861c173e30963a441dcafc30c35fa8925c
Carlos Mey <meycar96@gmail.com>
2023-02-02 10:01:23 -0500

    commit 1 deploy

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+import TheHeader from "@/components/TheHeader.vue";
+import EntryEditor from "./components/EntryEditor.vue";
+import EntryCard from "@/components/EntryCard.vue";
+import {reactive, provide, inject} from 'vue';
+import type User from "./types/User";
+import type Entry from "./types/Entry";
+import { userInjectionKey } from "./injectionKeys";
+const entries:Entry[] = reactive([]);
+const user: User = reactive({
+  id: 1,
+  username: "Carlos Mey",
+  settings: [],
+});
+provide(userInjectionKey, user);
+console.log(user.id);
+
+// in child component
+
+
+const handleCreateEntry = (entry: Entry) => {
+  entries.unshift(entry);
+  console.log(entry)
+}
+
+</script>
+
+<template>
+  <main class="container m-auto p-10">
+    <TheHeader />
+    <EntryEditor @@create="handleCreateEntry"/>
+    <ul>
+      <li v-for="entry in entries" :key="entry.id">
+        <EntryCard :entry="entry"/>
+      </li>
+    </ul>
+  </main>
+</template>
---
+++ src/components/DateDisplay.vue
@@ -0,0 +1,27 @@
+<script lang="ts">
+import formatRelative from "date-fns/formatRelative";
+import { defineComponent } from "vue";
+import type Entry from "@/types/Entry";
+import type { PropType } from "vue";
+export default defineComponent({
+  props: {
+    date: { type: Date, required: true },
+  },
+  emits:{
+    doSomething(payload:Entry){
+      return true;
+    }
+  },
+  computed: {
+    formatted():String {
+      return formatRelative(this.date, Date.now());
+    },
+  },
+  mounted(){
+    this.formatted;
+  }
+});
+</script>
+<template>
+  <div>{{ formatted }}</div>
+</template>
